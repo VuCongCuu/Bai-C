96705b073fcf4ae1b9cbd3c7554d03abf8a07316
VuCongCuu <congcuu1102@gmail.com>
2021-04-04 14:46:45 +0700

    dung sai

---
+++ bai tap C/true & false.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a,b,c,d;
+	
+	printf("a=\n");
+	scanf("%d",&a);
+	
+	printf ("b=\n");
+	scanf("%d",&b);
+	
+	printf("c=\n");
+	scanf("%d",&c);
+	
+	printf("d=\n");
+	scanf("%d",&d);
+	
+	if(a>b){ 
+	printf("true\n");
+	} else {
+		printf("false\n");
+	}
+	if((a+b)/2>(c+d)/2) {
+		printf("true\n");
+		} else {
+		printf("false\n");
+	}
+}
